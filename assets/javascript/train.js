6dd2a0fa8f4f4794d5df0ae283fda4b3e6bc05ed
unknown <monmaramba@yahoo.com>
2018-07-07 15:58:27 -0700

    it's alive! firebase push working, time computations are good

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -13,12 +13,60 @@ var config = {
   var database = firebase.database();
 //for current time and update every second
 function clock() {
-$("#currentTime").text(moment().format("D MMM YYYY HH:mm:ss"));
+$("#currentTime").text(moment().format("D MMM HH:mm:ss"));
 }
 setInterval(clock, 1000);
 //Initial values
+var now = moment().format("HH:mm");
+var tName = "";
+var destinationIn = "";
+var firstTIn = "";
+var freq;
+var timeDiff;
 
 //Button click to capture values for train
+$("#go").on("click", function(event){
+    event.preventDefault();
+
+    tName = $("#TrainNameIn").val().trim();
+    destination = $("#DestinationIn").val().trim();
+    firstTIn = $("#firstTrainIn").val().trim();
+    freq = $("#frequencyIn").val().trim();
+
+    //Code to push into firebase
+
+    database.ref().push({
+        name: tName,
+        destination: destination,
+        firstTrain: firstTIn,
+        frequency: freq
+    })
+
+
+    //computations for minutes til next train
+    //to make sure computation is for a future event    
+    var convertedTime = moment(firstTIn, "hh:mm").subtract(1, "years");
+        
+    timeDiff = moment().diff(moment(convertedTime), "minutes");
+    timeRemain = timeDiff % freq;
+
+    console.log(tName);
+    console.log(destination);
+    console.log(firstTIn);
+    console.log(timeRemain);
+
+
+
+
+
+})
+
+//to empty input forms
+$("#TrainNameIn").val("");
+$("#DestinationIn").val("");
+$("#firstTrainIn").val("");
+$("#frequencyIn").val("");
+
 
 //compute for next arrival and minutes away
 
